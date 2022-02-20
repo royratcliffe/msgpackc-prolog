d15c3445e979facc15906964d30a1187f2d141db
Roy Ratcliffe <royratcliffe@me.com>
2022-02-20 09:41:11 +0000

    msgpack_pack_object/2

--- c/msgpackc.c
+++ c/msgpackc.c
@@ -3,8 +3,73 @@
 
 #include "msgpack.h"
 
-foreign_t version1(term_t Version)
-{ return PL_unify_atom_chars(Version, msgpack_version());
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+static int
+stream_write(void *data, const char *buf, size_t len)
+{ return Sfwrite(buf, 1, len, data) != len;
+}
+
+/*
+ * Argument order of stream-object rather than the other way around
+ * mimics the write/2 predicate family where the stream comes first in
+ * the argument list.
+ *
+ * What the C implementation calls a message packer is simply
+ * encapsulates an opaque callback. It carries no other state. The
+ * recursive object pack function encodes the packing mechanisms.
+ */
+static foreign_t
+pack_object_2(term_t Stream, term_t Object)
+{ IOSTREAM *s;
+  msgpack_packer pk;
+  if (!PL_get_stream(Stream, &s, SIO_OUTPUT)) PL_fail;
+  if (s->encoding != ENC_OCTET)
+  { return PL_release_stream(s)
+        && PL_permission_error("msgpack_pack_object", "stream", Stream);
+  }
+  msgpack_packer_init(&pk, s, stream_write);
+  switch (PL_term_type(Object))
+  { case PL_NIL:
+      msgpack_pack_nil(&pk);
+      break;
+  }
+  return PL_release_stream(s);
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+                            _
+        __   _____ _ __ ___(_) ___  _ __
+        \ \ / / _ \ '__/ __| |/ _ \| '_ \
+         \ V /  __/ |  \__ \ | (_) | | | |
+          \_/ \___|_|  |___/_|\___/|_| |_|
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+static foreign_t
+version_string_1(term_t Version)
+{ return PL_unify_string_chars(Version, msgpack_version());
+}
+
+/*
+ * msgpack_version(?Version:term) is semidet.
+ *
+ * The Version term is a compound, or more specifically, a nested binary
+ * compound where the first argument unifies with the version Major and
+ * the sub-compound unifies with `:(Minor, Revision)` in Prolog.
+ *
+ *    A:B:C =.. [:, A, B:C].
+ */
+static foreign_t
+version_1(term_t Version)
+{ return PL_unify_term(Version,
+    PL_FUNCTOR_CHARS, ":", 2,
+      PL_INT, msgpack_version_major(),
+      PL_FUNCTOR_CHARS, ":", 2,
+        PL_INT, msgpack_version_minor(),
+        PL_INT, msgpack_version_revision());
 }
 
 /*
@@ -13,17 +78,22 @@ foreign_t version1(term_t Version)
  * its result. Failure here in the world of C means integer 0; any other
  * unification result indicates success.
  */
-foreign_t version3(term_t Major, term_t Minor, term_t Revision)
+static foreign_t
+version_3(term_t Major, term_t Minor, term_t Revision)
 { return PL_unify_integer(Major, msgpack_version_major())
       && PL_unify_integer(Minor, msgpack_version_minor())
       && PL_unify_integer(Revision, msgpack_version_revision());
 }
 
-install_t install_msgpackc()
-{ PL_register_foreign("msgpack_version", 1, version1, 0);
-  PL_register_foreign("msgpack_version", 3, version3, 0);
+install_t
+install_msgpackc()
+{ PL_register_foreign("msgpack_pack_object", 2, pack_object_2, 0);
+  PL_register_foreign("msgpack_version_string", 1, version_string_1, 0);
+  PL_register_foreign("msgpack_version", 1, version_1, 0);
+  PL_register_foreign("msgpack_version", 3, version_3, 0);
 }
 
-install_t uninstall_msgpackc()
+install_t
+uninstall_msgpackc()
 { ;
 }
--- prolog/msgpackc.pl
+++ prolog/msgpackc.pl
@@ -1,26 +1,63 @@
 /*  File:    msgpackc.pl
     Author:  Roy Ratcliffe
-    Created: Jan 19 2022
+    Created: Feb 19 2022
     Purpose: C-Based Message Pack for SWI-Prolog
 */
 
 :- module(msgpackc,
-          [ msgpack_version/1,          % ?Version
+          [ msgpack_pack_object/2,      % +Stream,+Term
+            msgpack_version_string/1,   % ?Version
+            msgpack_version/1,          % ?Version
             msgpack_version/3           % ?Major,?Minor,?Revision
           ]).
 :- use_foreign_library(foreign(msgpackc)).
 
-%!  msgpack_version(?Version:atom) is semidet.
-%!  msgpack_version(?Major:integer, ?Minor:integer, ?Revision:integer)
-%!  is semidet.
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+%!  msgpack_pack_object(+Stream, +Term) is det.
+%
+%   Throws a permission error if Stream does *not* have octet encoding.
+%   Hence packing to `current_output` or `user_output` fails since its
+%   encoding is typically something else, e.g. `wchar_t` on Windows or
+%   `utf8` on Linux flavours and macOS.
+%
+%       catch(msgpack_pack_object(user_output, []), error(A, B), true).
+%
+%   @throws permission_error
+
+:- begin_tests(msgpack_pack).
+
+test(permission_error, [error(permission_error(_, _, _))]) :-
+    msgpack_pack_object(current_output, []).
+
+:- end_tests(msgpack_pack).
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+%!  msgpack_version_string(?Version:string) is semidet.
 %
 %   Currently-deployed version of Message Pack C library. Use this both
 %   to access the current version atom, more typically, or else
 %   semi-deterministically to test against a version prerequisite.
 
+:- begin_tests(msgpack_version_string).
+
+test(version_string, [A == "4.0.0"]) :- msgpack_version_string(A).
+test(version_string, [fail]) :- msgpack_version_string("3.0.0").
+
+:- end_tests(msgpack_version_string).
+
+%!  msgpack_version(?Version) is semidet.
+%!  msgpack_version(?Major:integer, ?Minor:integer, ?Revision:integer)
+%!  is semidet.
+
 :- begin_tests(msgpack_version).
 
-test(version, [A == '4.0.0']) :- msgpack_version(A).
-test(version, [fail]) :- msgpack_version('3.0.0').
+test(version, [A:B:C == 4:0:0]) :- msgpack_version(A:B:C).
+test(version, [A-B-C == 4-0-0]) :- msgpack_version(A, B, C).
 
 :- end_tests(msgpack_version).
