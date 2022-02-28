ecb1b5cd023743af6d09d771692a99a272b98591
Roy Ratcliffe <royratcliffe@me.com>
2022-02-28 07:16:14 +0000

    Reorder predicates

--- .github/workflows/run_tests.pl
+++ .github/workflows/run_tests.pl
@@ -30,78 +30,6 @@ load_prolog_module(Directory, Module) :-
   catch(load_files(File, [must_be_module(true)]), _, fail),
   module_property(Module, file(File)).
 
-:- initialization(cov).
-
-cov :-
-    module_coverages(ModuleCoverages),
-    print_module_coverages(ModuleCoverages),
-    aggregate_all(
-        all(sum(Clauses), sum(Cov), sum(Fail), count),
-        member(_-coverage{
-                     clauses:Clauses,
-                     cov:Cov,
-                     fail:Fail
-                 }, ModuleCoverages),
-        all(AllClauses, AllCov, AllFail, AllModule)),
-    AvgCov is AllCov / AllModule,
-    AvgFail is AllFail / AllModule,
-    format('Modules:~t~d~40|~n', [AllModule]),
-    format('Clauses:~t~d~40|~n', [AllClauses]),
-    format('Cov:~t~f~40|%~n', [AvgCov]),
-    format('Fail:~t~f~40|%~n', [AvgFail]),
-    (   getenv('CANNY_COV_GIST_ID', GistID)
-    ->  shield_files([cov-AvgCov, fail-AvgFail], Files),
-        ghapi_update_gist(GistID, json(json([files=Files])), _, [])
-    ;   true
-    ).
-
-module_coverages(ModuleCoverages) :-
-    pack_info(., name(Pack)),
-    load_pack_modules(Pack, Modules),
-    findall(
-        Module-Coverage,
-        coverage_for_modules(run_tests, Modules, Module, Coverage),
-        ModuleCoverages).
-
-print_module_coverages(ModuleCoverages) :-
-    predsort(compare_cov_fail, ModuleCoverages, SortedModuleCoverages),
-    print_table(
-        member(
-            Module-coverage{
-                       clauses:Clauses,
-                       cov:Cov,
-                       fail:Fail
-                   }, SortedModuleCoverages), [Module, Clauses, Cov, Fail]).
-
-compare_cov_fail(Order, _-Coverage1, _-Coverage2) :-
-    compare(Order0, Coverage1.cov, Coverage2.cov),
-    compare_fail(Order, Order0, Coverage1.fail, Coverage2.fail),
-    !.
-compare_cov_fail(>, _, _).
-
-compare_fail(<, <, _, _) :- !.
-compare_fail(<, =, Fail1, Fail2) :- compare(>, Fail1, Fail2), !.
-compare_fail(>, _, _, _).
-
-shield_files(Pairs, json(Files)) :-
-    maplist([Label-Percent, File=json([content=Content])]>>
-            (   atom_concat(Label, '.json', File),
-                format(atom(Message), '~1f%', [Percent]),
-                shield_color(Percent, Color),
-                atom_json_term(Content, json([ schemaVersion=1,
-                                               label=Label,
-                                               message=Message,
-                                               color=Color
-                                             ]), []),
-                format('raw/~s~n', [File])
-            ), Pairs, Files).
-
-shield_color(Percent, red) :- Percent < 20, !.
-shield_color(Percent, orange) :- Percent < 40, !.
-shield_color(Percent, yellow) :- Percent < 60, !.
-shield_color(Percent, yellowgreen) :- Percent < 80, !.
-shield_color(_, green).
-
 %!  coverages_by_module(:Goal, -Coverages:dict) is det.
 %
 %   Calls Goal within show_coverage/1  while   capturing  the  resulting
@@ -225,3 +153,75 @@ print_border(Widths) :-
 
 zip([], [], []).
 zip([H1|T1], [H2|T2], [[H1, H2]|T]) :- zip(T1, T2, T).
+
+:- initialization(cov).
+
+cov :-
+    module_coverages(ModuleCoverages),
+    print_module_coverages(ModuleCoverages),
+    aggregate_all(
+        all(sum(Clauses), sum(Cov), sum(Fail), count),
+        member(_-coverage{
+                     clauses:Clauses,
+                     cov:Cov,
+                     fail:Fail
+                 }, ModuleCoverages),
+        all(AllClauses, AllCov, AllFail, AllModule)),
+    AvgCov is AllCov / AllModule,
+    AvgFail is AllFail / AllModule,
+    format('Modules:~t~d~40|~n', [AllModule]),
+    format('Clauses:~t~d~40|~n', [AllClauses]),
+    format('Cov:~t~f~40|%~n', [AvgCov]),
+    format('Fail:~t~f~40|%~n', [AvgFail]),
+    (   getenv('CANNY_COV_GIST_ID', GistID)
+    ->  shield_files([cov-AvgCov, fail-AvgFail], Files),
+        ghapi_update_gist(GistID, json(json([files=Files])), _, [])
+    ;   true
+    ).
+
+module_coverages(ModuleCoverages) :-
+    pack_info(., name(Pack)),
+    load_pack_modules(Pack, Modules),
+    findall(
+        Module-Coverage,
+        coverage_for_modules(run_tests, Modules, Module, Coverage),
+        ModuleCoverages).
+
+print_module_coverages(ModuleCoverages) :-
+    predsort(compare_cov_fail, ModuleCoverages, SortedModuleCoverages),
+    print_table(
+        member(
+            Module-coverage{
+                       clauses:Clauses,
+                       cov:Cov,
+                       fail:Fail
+                   }, SortedModuleCoverages), [Module, Clauses, Cov, Fail]).
+
+compare_cov_fail(Order, _-Coverage1, _-Coverage2) :-
+    compare(Order0, Coverage1.cov, Coverage2.cov),
+    compare_fail(Order, Order0, Coverage1.fail, Coverage2.fail),
+    !.
+compare_cov_fail(>, _, _).
+
+compare_fail(<, <, _, _) :- !.
+compare_fail(<, =, Fail1, Fail2) :- compare(>, Fail1, Fail2), !.
+compare_fail(>, _, _, _).
+
+shield_files(Pairs, json(Files)) :-
+    maplist([Label-Percent, File=json([content=Content])]>>
+            (   atom_concat(Label, '.json', File),
+                format(atom(Message), '~1f%', [Percent]),
+                shield_color(Percent, Color),
+                atom_json_term(Content, json([ schemaVersion=1,
+                                               label=Label,
+                                               message=Message,
+                                               color=Color
+                                             ]), []),
+                format('raw/~s~n', [File])
+            ), Pairs, Files).
+
+shield_color(Percent, red) :- Percent < 20, !.
+shield_color(Percent, orange) :- Percent < 40, !.
+shield_color(Percent, yellow) :- Percent < 60, !.
+shield_color(Percent, yellowgreen) :- Percent < 80, !.
+shield_color(_, green).
