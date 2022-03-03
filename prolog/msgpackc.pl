61c3cd9805c23c416deb527b986a80ba5319c568
Roy Ratcliffe <royratcliffe@me.com>
2022-03-03 21:07:56 +0000

    8-bit fixed integers

--- prolog/msgpackc.pl
+++ prolog/msgpackc.pl
@@ -16,18 +16,60 @@
 %   object as follows.
 %
 %       1. The _nil_ object becomes Prolog `nil` atom rather than `[]`
-%       which Prolog calls nil, the empty list termination. Prolog `[]`
+%       which Prolog calls "nil," the empty list termination. Prolog `[]`
 %       decodes an empty Message Pack array.
 %       2. Booleans become Prolog atoms `false` and `true`.
 %       3. Integers become Prolog integers.
-%       4. Floats become Prolog floats.
-%       2. Strings in UTF-8 become Prolog strings, never atoms.
-%       2. Arrays become Prolog lists.
-%       3. Maps become Prolog dictionaries.
+%
+%       4. Floats become Prolog floats. Distinguishing between 32- and
+%       64-bit float-point occurs by wrapping the Prolog-side in
+%       float(Precision, Number) terms where Precision selects 32 or 64
+%       bits. Setting up an epsilon threshold allows for automatic
+%       precision adjustment when encoding.
+%
+%       5. Strings in UTF-8 become Prolog strings, never atoms.
+%       6. Arrays become Prolog lists.
+%       7. Maps become Prolog dictionaries.
 
-msgpack_object(nil) --> [0xc0].
-msgpack_object(false) --> [0xc2].
-msgpack_object(true) --> [0xc3].
+msgpack_object(nil) --> [0xc0], !.
+msgpack_object(false) --> [0xc2], !.
+msgpack_object(true) --> [0xc3], !.
+msgpack_object(Byte) -->
+    byte(Byte),
+    { Byte =< 0x7f
+    },
+    !.
+msgpack_object(Integer) -->
+    fixint(8, Integer).
+
+fixint(8, Integer) -->
+    { var(Integer)
+    },
+    byte(Byte),
+    { Byte >= 0xe0,
+      Integer is Byte - 0x100
+    },
+    !.
+fixint(8, Integer) -->
+    { integer(Integer),
+      Byte is 0x100 + Integer,
+      Byte >= 0xe0
+    },
+    byte(Byte).
+
+%!  byte(Byte)// is semidet.
+%
+%   Simplifies the Message Pack grammar by asserting Byte constraints.
+%   Every Byte is an integer in-between 0 and 255 inclusive. Other
+%   high-level grammer components can presume these contraints as a
+%   baseline and assert any addition limits appropriately.
+
+byte(Byte) -->
+    [Byte],
+    { integer(Byte),
+      Byte >= 0x00,
+      Byte =< 0xff
+    }.
 
 %!  msgpack_objects(?Objects)// is semidet.
 %
--- prolog/msgpackc.plt
+++ prolog/msgpackc.plt
@@ -6,6 +6,11 @@ test(msgpack_object_nil, [true(A == [0xc0])]) :-
 test(msgpack_object_nil, [true(A == nil)]) :-
     phrase(msgpack_object(A), [0xc0]).
 
+test(msgpack_object_fixint, [true(A == [0xff])]) :-
+    phrase(msgpack_object(-1), A).
+test(msgpack_object_fixint, [true(A == [0x7f])]) :-
+    phrase(msgpack_object(127), A).
+
 test(msgpack_objects, [true(A == [nil, false, true])]) :-
     phrase(msgpack_objects(A), [0xc0, 0xc2, 0xc3]).
 test(msgpack_objects, [true(A == [0xc0, 0xc2, 0xc3])]) :-
