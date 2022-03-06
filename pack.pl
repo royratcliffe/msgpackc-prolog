b822bcf72710ebdd228f0028eae85de85c31a905
Roy Ratcliffe <royratcliffe@me.com>
2022-03-06 13:51:30 +0000

    Download from upstream swipl/msgpackc (#5)

--- pack.pl
+++ pack.pl
@@ -1,8 +1,8 @@
 name(msgpackc).
-version('0.1.0').
+version('0.1.1').
 title('C-Based Message Pack for SWI-Prolog').
 author('Roy Ratcliffe', 'royratcliffe@me.com').
 packager('Roy Ratcliffe', 'royratcliffe@me.com').
 maintainer('Roy Ratcliffe', 'royratcliffe@me.com').
-home('https://github.com/royratcliffe/msgpackc-prolog').
-download('https://github.com/royratcliffe/msgpackc-prolog/releases/*.zip').
+home('https://github.com/swipl/msgpackc').
+download('https://github.com/swipl/msgpackc/releases/*.zip').
