61b2b39b9e57ffcecb8a19cf57cf235282339bfd
Roy Ratcliffe <royratcliffe@me.com>
2022-03-06 12:03:19 +0000

    Author, home and download

--- pack.pl
+++ pack.pl
@@ -1,3 +1,8 @@
 name(msgpackc).
 version('0.1.0').
 title('C-Based Message Pack for SWI-Prolog').
+author('Roy Ratcliffe', 'royratcliffe@me.com').
+packager('Roy Ratcliffe', 'royratcliffe@me.com').
+maintainer('Roy Ratcliffe', 'royratcliffe@me.com').
+home('https://github.com/royratcliffe/msgpackc-prolog').
+download('https://github.com/royratcliffe/msgpackc-prolog/releases/*.zip').
